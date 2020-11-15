49557eac97d256dda7d4a9709867fdc789a26d52
maleke <malekelthomas97@gmail.com>
2020-11-15 04:42:12 -0500

    added post method

--- src/main/java/com/vinivender/vending/product/ProductController.java
+++ src/main/java/com/vinivender/vending/product/ProductController.java
@@ -1,4 +1,6 @@
 package com.vinivender.vending.product;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -6,7 +8,10 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,12 +21,23 @@ public class ProductController{
     private ProductService productService;
 
     @RequestMapping("/products")
-    public Map<String, Product[]> getAllProducts(){
+    public Map<String, ArrayList<Product>> getAllProducts(){
         return this.productService.getAllProducts();
     }
+
     @RequestMapping("/products/{id}")
-    public Product getProduct(@PathVariable String id){
+    public Product getProduct(@PathVariable Integer id){
         return this.productService.getProduct(id);
+    }
+    @RequestMapping(method = RequestMethod.POST, value ="/products")
+    @ResponseBody
+    public void addProduct(@RequestBody ProductDTO product){
+        Product persistentProduct = new Product();
+        persistentProduct.setId(product.getId());
+        persistentProduct.setProductName(product.getProductName());
+        persistentProduct.setPrice(product.getPrice());
+        persistentProduct.setDescription(product.getDescription());
+        this.productService.addProduct(persistentProduct);
     }
     
 }
