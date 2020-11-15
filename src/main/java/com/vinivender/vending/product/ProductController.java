cb8aed76458a1f967c2169904204cdac4dadd030
maleke <malekelthomas97@gmail.com>
2020-11-14 20:30:05 -0500

    Added Product Controller

--- src/main/java/com/vinivender/vending/product/ProductController.java
+++ src/main/java/com/vinivender/vending/product/ProductController.java
@@ -4,19 +4,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController{
 
+    @Autowired
+    private ProductService productService;
+
     @RequestMapping("/products")
     public Map<String, Product[]> getAllProducts(){
-        Map<String, Product[]> productsData = new HashMap<String, Product[]>();
-        Product[] products = {new Product("1","Doritos","$1.00","Nacho Cheese"), new Product("2","Doritos","$1.00","Cool Ranch")}; 
-        productsData.put("data", products);
-
-        return productsData;        
+        return this.productService.getAllProducts();
     }
     
 }
