277620cea88bf624170c1dd0f46199ff4e6f3d3e
maleke <malekelthomas97@gmail.com>
2020-11-21 02:36:43 -0500

    Enabled eureka client

--- main-app-react/frontend/src/main/java/com/vending/frontend/FrontendApplication.java
+++ main-app-react/frontend/src/main/java/com/vending/frontend/FrontendApplication.java
@@ -2,6 +2,7 @@ package com.vending.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
