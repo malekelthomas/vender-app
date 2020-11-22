11661c86cf34b82b66413bec8de952bd8bbf6141
maleke <malekelthomas97@gmail.com>
2020-11-22 05:46:50 -0500

    Updated

--- main-app-react/frontend/src/main/js/app.js
+++ main-app-react/frontend/src/main/js/app.js
@@ -1,8 +1,9 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import Container from './Container';
+
 
 
 ReactDOM.render(
