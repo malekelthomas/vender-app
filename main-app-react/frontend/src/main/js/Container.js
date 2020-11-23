ce8311e3d1a0fe5ee432db91735e59beec9f4b5f
maleke <malekelthomas97@gmail.com>
2020-11-23 01:27:35 -0500

    Added login and register routes

--- main-app-react/frontend/src/main/js/Container.js
+++ main-app-react/frontend/src/main/js/Container.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './css/Container.css';
 import Product from './Product';
 import VendingMachine from './VendingMachine';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
 import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
 import { AppBar, Menu, MenuItem, Typography } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
@@ -34,9 +36,21 @@ const Container = () => {
                             <Typography className={classes.title} variant="h6" noWrap>Vending Machine 1</Typography>
                         </Link>
                     </MenuItem>
+                    <MenuItem>
+                        <Link to="/login">
+                            <Typography className={classes.title} variant="h6" noWrap>Login</Typography>
+                        </Link>
+                    </MenuItem> 
+                    <MenuItem>
+                        <Link to="/register">
+                            <Typography className={classes.title} variant="h6" noWrap>Register</Typography>
+                        </Link>
+                    </MenuItem>
                 </AppBar>
             
                 <Switch>
+                    <Route path="/login" component={LoginForm}></Route>
+                    <Route path="/register" component={RegisterForm}></Route>
                     <Route path="/vendingmachines/:id?" component={VendingMachine}></Route>
                     <Route path="/products/:id?" component={Product}></Route>
                 </Switch>
